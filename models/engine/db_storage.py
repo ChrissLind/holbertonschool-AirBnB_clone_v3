3fd4b2f841e0bccef19e4d7924c969c88f0ce378
ChrissLind <chrislindino@gmail.com>
2022-09-16 13:23:14 -0400

    first commit

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieves an object"""
+        if cls and id:
+            fetch = "{}.{}".format(cls, id)
+            all_obj = self.all(cls)
+            return all_obj.get(fetch)
+        return None
+
+    def count(self, cls=None):
+        """Counts number of objects"""
+        return (len(self.all(cls)))
